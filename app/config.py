a59e0ce28c26c6332d7184236ef5381f8a56d380
bouchramilou <miloudybouchra01@gmail.com>
2026-02-03 15:17:44 +0100

    MED-26 Dockerisation

--- app/config.py
+++ app/config.py
@@ -1,3 +1,30 @@
-# Gestion de la configuration
-# Lecture des variables .env (JWT, DB, LLM, etc.)
-# Utilise pydantic-settings
+from pydantic import model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "MediAssist API"
+    VERSION: str = "1.0.0"
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str
+    
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str
+    DATABASE_URL: str | None = None
+    
+    ENVIRONMENT: str = "dev"
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+    @model_validator(mode="after")
+    def assemble_db_connection(self):
+        if not self.DATABASE_URL:
+            self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return self
+
+settings = Settings()
